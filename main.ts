3c140ac44544d2601d479976ff08d1ee1df7d80d
Mangelmango <123979961+Mangelmango@users.noreply.github.com>
2023-01-30 12:12:06 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+    }
+})
